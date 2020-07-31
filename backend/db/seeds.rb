4a32b6956d729c1aa84096d7566f0279b578f9b8
isaac-3 <isaacgc0596@gmail.com>
2020-07-30 22:58:56 -0500

    links

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -4,11 +4,12 @@ Blog.destroy_all
 
 u1 = User.create({username: 'one', password: '123', name: 'name'})
 
-Project.create({name: "one", user_id: u1.id, project_url: 'youtube.com'})
-Project.create({name: "two", user_id: u1.id, project_url: 'youtube.com'})
-Project.create({name: "three", user_id: u1.id, project_url: 'youtube.com'})
+Project.create({name: "Take A Trip", user_id: u1.id, project_url: 'https://www.youtube.com/watch?v=adlCActjmRc&feature=youtu.be'})
+Project.create({name: "Shoptastic", user_id: u1.id, project_url: 'https://www.youtube.com/watch?v=QG8HfCflFWM&feature=youtu.be'})
+Project.create({name: "Chrello", user_id: u1.id, project_url: 'https://www.youtube.com/watch?v=jqrZRoTbMS8'})
 
-Blog.create({name: 'one', user_id: u1.id, blog_url: 'youtube.com'})
-Blog.create({name: 'two', user_id: u1.id, blog_url: 'youtube.com'})
-Blog.create({name: 'three', user_id: u1.id, blog_url: 'youtube.com'})
-
+Blog.create({name: 'Improve Your Typing', user_id: u1.id, blog_url: 'https://medium.com/@isaacgc0596/improve-your-typing-48f7e795e1ef'})
+Blog.create({name: 'Event Listeners', user_id: u1.id, blog_url: 'https://medium.com/@isaacgc0596/event-listeners-878a0d6fe101'})
+Blog.create({name: 'Finding The Best Route', user_id: u1.id, blog_url: 'https://medium.com/@isaacgc0596/finding-the-best-route-34e4b752ae58'})
+Blog.create({name: 'What Time Is It', user_id: u1.id, blog_url: 'https://medium.com/@isaacgc0596/what-time-is-it-2e353667291c'})
+Blog.create({name: 'Choosing Paths Through Control Flow', user_id: u1.id, blog_url: 'https://medium.com/@isaacgc0596/choosing-paths-through-control-flow-50477255c055'})
--- frontend/src/components/ProjCard.js
+++ frontend/src/components/ProjCard.js
@@ -68,3 +68,5 @@ export default function ImgMediaCard(props) {
     // </Card>
   );
 }
+
+
