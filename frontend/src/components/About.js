7167ebc53da795cfbaa81f9347c748746fc50819
isaac-3 <isaacgc0596@gmail.com>
2020-08-05 23:28:39 -0500

    word effect

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,6 +4,20 @@ const About = () => {
     return (
         <div style={{height: '100vh', backgroundColor: 'rgb(105, 105, 105)', textAlign: 'center'}} id="about">
             <div style={{paddingTop: '72px'}}>
+            <div className='wavy'>
+              <span style={{isolation: 1}}>I</span>
+              <span style={{isolation: 2}}>S</span>
+              <span style={{isolation: 3}}>A</span>
+              <span style={{isolation: 4}}>A</span>
+              <span style={{isolation: 5}}>C</span>
+
+              <span style={{isolation: 6}}>C</span>
+              <span style={{isolation: 7}}>H</span>
+              <span style={{isolation: 8}}>A</span>
+              <span style={{isolation: 9}}>V</span>
+              <span style={{isolation: 10}}>E</span>
+              <span style={{isolation: 11}}>Z</span>
+            </div>
                 <h1>About Me</h1>
                 <h3>My Names Isaac and I have passion for software development</h3>
             </div>
