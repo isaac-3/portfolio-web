d39f7cbc327d9063ebdce4afb013adb89d43a425
isaac-3 <isaacgc0596@gmail.com>
2020-08-17 23:46:05 -0500

    contact me form

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -5,20 +5,36 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Contact = () => {
     return (
-        <div style={{height: '100vh', textAlign: 'center'}} id="contact">
-            <div style={{paddingTop: '72px'}}>
+        <div className="contact-sec" style={{height: '100vh', textAlign: 'center', backgroundColor: '#ebecf0'}} id="contact">
+            {/* <div style={{paddingTop: '72px'}}>
+            </div> */}
+            <div style={{textAlign: 'left'}}>
                 <h1>Contact Me</h1>
+                <div className="contact-icon">
+                    <i class="fa fa-envelope-o" id='email-icon' onClick={()=>window.open("mailto:isaacgc0596@gmail.com?subject=Hello!")} style={{fontSize:"36px"}}></i>
+                    <span style={{marginLeft: "8px"}}>isaacgc0596@gmail.com</span><br/>
+                </div>
+                <div className="contact-icon">
+                    <i class="fa fa-phone" id="phone-icon" style={{fontSize:"36px"}}></i>
+                    <span style={{marginLeft: "8px"}}>(281) 935-5556</span>
+                </div>
+                <div className="contact-icon">
+                    <i class="fa fa-linkedin-square" id='linkedin-icon' onClick={()=>window.open("https://www.linkedin.com/in/isaac-c0/")} style={{fontSize:"36px"}}></i>
+                    <span style={{marginLeft: "8px"}}>Connect With Me</span><br/>
+                </div>
+                <div className="contact-icon">
+                    <i class="fa fa-github" id='github-icon' onClick={()=>window.open("https://github.com/isaac-3")} style={{fontSize:"36px"}}></i>
+                    <span style={{marginLeft: "8px"}}>GitHub Profile</span>
+                </div>
             </div>
-            <div style={{textAlign: 'center', paddingLeft: '40%'}}>
-                <div style={{float: 'left'}} onClick={()=>window.open("mailto:isaacgc0596@gmail.com?subject=Hello!")}>
-                <MailOutlineOutlinedIcon style={{fontSize: '72px', paddingRight: '8px', paddingLeft: '8px'}}/>
+            <div className="contact-form">
+                <div className="form-title">
+                    <h1>Get In Touch</h1>
                 </div>
-                <div style={{float: 'left'}} onClick={()=>window.open("https://www.linkedin.com/in/isaac-c0/")}>
-                <LinkedInIcon style={{fontSize: '72px', paddingRight: '8px', paddingLeft: '8px'}}/>
-                </div>
-                <div style={{float: 'left'}} onClick={()=>window.open("https://github.com/isaac-3")}>
-                <GitHubIcon style={{fontSize: '56px', paddingBottom: "11px", paddingRight: '8px', paddingLeft: '8px'}}/>
-                </div>
+                <label className='label'>Name</label><input className='input' type='text' placeholder="First Last" name="name"/><br/><br/>
+                <label className='label'>Email</label><input className='input' type='text' placeholder="ex@example.com" name="email"/><br/><br/>
+                <label className='label'>Message</label><textarea className='input' id='message-box' type='text' placeholder="Enter Message" name="message"/><br/><br/>
+                <button className="red" type="button">Send</button>
             </div>
         </div>
     );
